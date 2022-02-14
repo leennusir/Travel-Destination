0aa4c003953dba961741eda807aa970faf042a8a
leen nusir <leen.nsser99@gmail.com>
2022-02-14 11:07:08 +0200

    edit div and line manner

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -5,9 +5,12 @@ function ListData(props) {
            props.infoData.map(val =>{
                 return (
                     <div>
+                      
         <p>name: {val.name}</p> 
         <img src={val.image}  width="500" height="600"></img>
+                  <hr/>
                     </div>
+                    
                 )
       })
       }
